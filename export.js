f594b96eeeaacd3859c7b77cc66915832c8b7ca0
janaleible <jana.leible@web.de>
2018-02-06 21:02:12 +0100

    use proper ECMAScript version

--- export.js
+++ export.js
@@ -1,17 +1,17 @@
 "use strict";
 
-var xmlParser = require('xml2js').parseString;
-var fileSystem = require('fs');
-var ics = require('ics');
-var moment = require('moment');
-var uuid = require('uuid');
+let xmlParser = require('xml2js').parseString;
+let fileSystem = require('fs');
+let ics = require('ics');
+let moment = require('moment');
+let uuid = require('uuid');
 
-// var url = 'https://webapi.login.vu.nl/api/study/Events/51505135/EN/29-01-2018/01-04-2018';
+// let url = 'https://webapi.login.vu.nl/api/study/Events/51505135/EN/29-01-2018/01-04-2018';
 
-var xml = fileSystem.readFile('./advanced-logic.xml', {encoding: 'utf8'}, function (error, xml) {
-    xmlParser(xml, function (error, schedule) {
+let xml = fileSystem.readFile('./advanced-logic.xml', {encoding: 'utf8'}, function (error, xml) {
+    xmlParser(xml, (error, schedule) => {
         ics.createCalendar(
-            schedule.ArrayOfEvent.Event.map(function (event) {
+            schedule.ArrayOfEvent.Event.map((event) => {
                 return {
                     title: event.Title[0] + ' (' + event.Type[0] + ')',
                     start: parseDate(event.Start[0]),
@@ -21,7 +21,7 @@ var xml = fileSystem.readFile('./advanced-logic.xml', {encoding: 'utf8'}, functi
                     location: event.Locations[0].Location[0].Code[0],
                     uid: uuid.v4()
                 };
-            }), {}, function (error, calendar) {
+            }), {}, (error, calendar) => {
                 fileSystem.writeFile('calendar.ics', calendar)
             }
         );
