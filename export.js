db87fd043335aac93baea5859b53cb928cf657c5
janaleible <jana.leible@web.de>
2018-02-06 20:58:37 +0100

    use experimental createCalendar() function

--- export.js
+++ export.js
@@ -3,28 +3,28 @@
 var xmlParser = require('xml2js').parseString;
 var fileSystem = require('fs');
 var ics = require('ics');
-var moment = require('moment')
+var moment = require('moment');
+var uuid = require('uuid');
 
 // var url = 'https://webapi.login.vu.nl/api/study/Events/51505135/EN/29-01-2018/01-04-2018';
 
 var xml = fileSystem.readFile('./advanced-logic.xml', {encoding: 'utf8'}, function (error, xml) {
     xmlParser(xml, function (error, schedule) {
-        schedule.ArrayOfEvent.Event
-        .map(function (event) {
-            return {
-                title: event.Title[0] + ' (' + event.Type[0] + ')',
-                start: parseDate(event.Start[0]),
-                duration: {
-                    minutes: getDuration(event.Start[0], event.End[0])
-                },
-                location: event.Locations[0].Location[0].Code[0]
-            };
-        })
-        .map(function (event, index) {
-            ics.createEvent(event, function (error, calendar) {
-                fileSystem.writeFile('calendar' + index + '.ics', calendar)
-            })
-        });
+        ics.createCalendar(
+            schedule.ArrayOfEvent.Event.map(function (event) {
+                return {
+                    title: event.Title[0] + ' (' + event.Type[0] + ')',
+                    start: parseDate(event.Start[0]),
+                    duration: {
+                        minutes: getDuration(event.Start[0], event.End[0])
+                    },
+                    location: event.Locations[0].Location[0].Code[0],
+                    uid: uuid.v4()
+                };
+            }), {}, function (error, calendar) {
+                fileSystem.writeFile('calendar.ics', calendar)
+            }
+        );
     });
 });
 
